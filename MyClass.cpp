d69a3be1e77c50c1e363b583fd81dccffa5787b8
Adrian Baptista <adrianbaptistap@gmail.com>
2021-02-14 00:20:32 -0500

    Add project files.

---
+++ MyClass.cpp
@@ -0,0 +1,15 @@
+#include "MyClass.h"
+MyClass::MyClass()
+{
+  //Constructor
+}
+
+string MyClass::foo()
+{
+	return "hola";
+}
+
+void MyClass::correr()
+{
+	cout << "Correr" << endl;
+}
---
+++ MyClass.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class MyClass
+{
+public:
+	MyClass();
+	string foo();
+	void correr();
+	void sayHello()
+	{
+		cout << "Hello" << endl;
+	}
+};
+
